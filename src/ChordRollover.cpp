6d956cabc78bbb5453febd82c676a515abb69140
KitchenMishap <hello@kitchenmishap.art>
2025-09-25 10:43:43 +0100

    Four params, and get rid of example interface widgets

--- src/ChordRollover.cpp
+++ src/ChordRollover.cpp
@@ -3,32 +3,40 @@
 
 struct ChordRollover : Module {
 	enum ParamId {
-		EXAMPLE_PARAM,
+		KEYSIG_PARAM,
+		CHORD_PARAM,
+		TIME_PARAM,
+		PROFILE_PARAM,
 		PARAMS_LEN
 	};
 	enum InputId {
-		EXAMPLE_INPUT,
 		VOCT_INPUT,
 		GATE_INPUT,
 		INPUTS_LEN
 	};
 	enum OutputId {
-		EXAMPLE_OUTPUT,
 		VOCT_OUTPUT,
 		GATE_OUTPUT,
 		OUTPUTS_LEN
 	};
 	enum LightId {
-		EXAMPLE_LIGHT,
 		ROLLOVER_LIGHT,
 		LIGHTS_LEN
 	};
 
+	enum Profiles {
+		STEP_PROFILE = 0,
+		TRIANGLE_PROFILE,
+		SINE_PROFILE,
+		HALFSINE_PROFILE
+	};
+
 	ChordRollover() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(EXAMPLE_PARAM, 0.f, 1.f, 0.f, "");
-		configInput(EXAMPLE_INPUT, "");
-		configOutput(EXAMPLE_OUTPUT, "");
+		configSwitch(KEYSIG_PARAM, 0.f, 11.f, 0.f, "Key Signature", {"C", "C♯/D♭", "D", "D♯/E♭", "E", "F", "F♯/G♭", "G", "G♯/A♭", "A", "A♯/B♭", "B"});
+		configSwitch(CHORD_PARAM, 1.f, 7.f, 4.f, "Notes in chord", {"Monad", "Diad", "Triad", "Tetrad", "Pentad", "Hexad", "Heptad"});
+		configParam(TIME_PARAM, 0.f, 5.f, 0.5f, "Glide time (s)" );
+		configSwitch(PROFILE_PARAM, (float)STEP_PROFILE, (float)HALFSINE_PROFILE, (float)TRIANGLE_PROFILE, "Glide profile", {"Step (Bypass)", "Triangle", "Sine", "Half Sine"});
 		configInput(VOCT_INPUT, "(Mono) Pitch");
 		configInput(GATE_INPUT, "(Mono) Gate");
 		configOutput(VOCT_OUTPUT, "(Poly) Pitch");
@@ -52,17 +60,17 @@ struct ChordRolloverWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(7.735, 16.277)), module, ChordRollover::EXAMPLE_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(width/2.f, 24)), module, ChordRollover::KEYSIG_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(width/2.f, 41)), module, ChordRollover::CHORD_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(width/2.f, 58)), module, ChordRollover::TIME_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(width/2.f, 75)), module, ChordRollover::PROFILE_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.513, 35.36)), module, ChordRollover::EXAMPLE_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7, 97)), module, ChordRollover::VOCT_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(width - 7, 97)), module, ChordRollover::GATE_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(7.606, 52.759)), module, ChordRollover::EXAMPLE_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(7, 113)), module, ChordRollover::VOCT_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(width - 7, 113)), module, ChordRollover::GATE_OUTPUT));
 
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(7.325, 70.813)), module, ChordRollover::EXAMPLE_LIGHT));
 		addChild(createLightCentered<MediumLight<GreenLight>>(mm2px(Vec(width - 7, 87)), module, ChordRollover::ROLLOVER_LIGHT));
 
 		// mm2px(Vec(10.0, 10.0))
