861443bb2e534ec40dac7f9d97275855ac4e91e1
KitchenMishap <hello@kitchenmishap.art>
2025-09-25 09:25:07 +0100

    Position and configure VOCT and GATE outputs

--- src/ChordRollover.cpp
+++ src/ChordRollover.cpp
@@ -12,6 +12,8 @@ struct ChordRollover : Module {
 	};
 	enum OutputId {
 		EXAMPLE_OUTPUT,
+		VOCT_OUTPUT,
+		GATE_OUTPUT,
 		OUTPUTS_LEN
 	};
 	enum LightId {
@@ -24,6 +26,8 @@ struct ChordRollover : Module {
 		configParam(EXAMPLE_PARAM, 0.f, 1.f, 0.f, "");
 		configInput(EXAMPLE_INPUT, "");
 		configOutput(EXAMPLE_OUTPUT, "");
+		configOutput(VOCT_OUTPUT, "Pitch");
+		configOutput(GATE_OUTPUT, "Gate");
 	}
 
 	void process(const ProcessArgs& args) override {
@@ -36,6 +40,8 @@ struct ChordRolloverWidget : ModuleWidget {
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/Panel.svg")));
 
+		float width = 5.08 * 5;
+
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
@@ -46,6 +52,10 @@ struct ChordRolloverWidget : ModuleWidget {
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.513, 35.36)), module, ChordRollover::EXAMPLE_INPUT));
 
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(7.606, 52.759)), module, ChordRollover::EXAMPLE_OUTPUT));
+
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(7, 113)), module, ChordRollover::VOCT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(width - 7, 113)), module, ChordRollover::GATE_OUTPUT));
+
 
 		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(7.325, 70.813)), module, ChordRollover::EXAMPLE_LIGHT));
 
