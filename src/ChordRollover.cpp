d47b2a3305b4132a75faaf65ecb2adc6103b5188
KitchenMishap <hello@kitchenmishap.art>
2025-09-25 08:33:38 +0100

    Remove components layer from Panel.svg; use Panel.svg

--- src/ChordRollover.cpp
+++ src/ChordRollover.cpp
@@ -34,7 +34,7 @@ struct ChordRollover : Module {
 struct ChordRolloverWidget : ModuleWidget {
 	ChordRolloverWidget(ChordRollover* module) {
 		setModule(module);
-		setPanel(createPanel(asset::plugin(pluginInstance, "res/InkscapeExampleVcv.svg")));
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/Panel.svg")));
 
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
